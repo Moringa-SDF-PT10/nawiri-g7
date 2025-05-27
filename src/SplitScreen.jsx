f699e293871c0e9ae61f23f3a20454a66a809ff2
Ellis Lunayo <ellislunayo@gmail.com>
2025-05-27 22:13:15 +0300

    Created a component that will hold the landing page with a split screen view

---
+++ src/SplitScreen.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import SplitScreen from "./SplitScreen";
+
+function SplitScreen () {
+    return (
+        <div className="App">
+            <h1>Split Screen Example</h1>
+            
+            
+        </div>
+    );
+}
+export default SplitScreen
