499c409609a5bdee6b2aa927817f6a5a5cc3908f
Ellis Lunayo <ellislunayo@gmail.com>
2025-05-27 23:43:09 +0300

    Installed the browser router dom for our contact page and other page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import SplitScreen from './SplitScreen';
 import nariri_school from './assets/nariri_school.jpg';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export default function App() {
   return (
