75de97b22f44fd79db9c3ea71088647a8b9a39ce
Douglas Gatimu <douglasgatimu@gmail.com>
2025-05-29 20:21:42 +0300

    merged test-login to main

--- src/router.jsx
+++ src/router.jsx
@@ -7,9 +7,31 @@ import App from "./App";
 import ForgotPassword from "./auth/ForgotPassword";
 import PlaceholderProfile from "./PlaceholderProfile";
 import PrivateRoute from "./PrivateRoutes";
+import Layout from "./Layout";
+import SplitScreen from "./SplitScreen";
+import ContactPage from "./ContactPage";
+import About from "./About";
+
 
 export const router = createBrowserRouter([
   { path: "/", element: <App /> },
+    {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <SplitScreen />,
+      },
+      {
+        path: '/contact',
+        element: <ContactPage />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+    ],
+  },
   { path: "/signup", element: <SignUpPage /> },
   { path: "/signin", element: <SignInPage /> },
   { path: "/forgot-password", element: <ForgotPassword /> },
